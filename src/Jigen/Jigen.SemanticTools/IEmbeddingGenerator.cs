0ca0eb9fae9da4ff235af2df067d575844860d68
Paolo Possanzini <paolo@teamdev.it>
2026-02-10 12:31:01 +0100

    jigen moved to new repo

--- src/Jigen/Jigen.SemanticTools/IEmbeddingGenerator.cs
+++
@@ -1,6 +0,0 @@
-namespace Jigen.SemanticTools;
-
-public interface IEmbeddingGenerator
-{
-  float[] GenerateEmbedding(string input);
-}
--- src/Jigen/Jigen/DataStructures/StoreHeader.cs
+++
@@ -1,9 +0,0 @@
-namespace Jigen.DataStructures;
-
-public class StoreHeader
-{
-
-
-  public long EmbeddingCurrentPosition { get; set; }
-  public long ContentCurrentPosition { get; set; }
-}
